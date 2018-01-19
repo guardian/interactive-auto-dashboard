c92dc98236bad0e4cce001121d379a1c0989bc6b
seanclarkeguardian <sean.clarke@guardian.co.uk>
2018-01-19 11:22:28 +0000

    changes severall

--- src/js/app.js
+++ src/js/app.js
@@ -7,22 +7,18 @@ import * as d3scale from 'd3-scale'
 import * as d3request from 'd3-request'
 import * as d3array from 'd3-array'
 import * as d3axis from 'd3-axis'
+import config from './../../config.json'
 
+var dataurl = config.docDataUrl;
 
-var dataurl = "https://interactive.guim.co.uk/docsdata-test/1mm9nd1wnyE-YOiOk26cN-jhxqUuRWb1eFOrnnKqte14.json";
+function drawlinechart(data, selector, ticks, zeroy, interval, destination, chartType, numberOfDataSeries) {
 
-
-
-function drawlinechart(data, selector, ticks, zeroy, interval, destination, chartType,numberOfDataSeries) {
-
-    var destinationdiv = window.parent.document.querySelector('figure[data-alt="' + destination +'"]');
+    var destinationdiv = window.parent.document.querySelector('figure[data-alt="' + destination + '"]');
     if (destinationdiv != null) {
         var destwidth = destinationdiv.clientWidth;
     }
 
-    console.log(destwidth)
-
-    var svg = select("svg"+selector), margin = { top: 20, right: 20, bottom: 20, left: 40 },
+    var svg = select("svg" + selector), margin = { top: 20, right: 20, bottom: 20, left: 40 },
         // width = +svg.attr("width") - margin.left - margin.right,
         // height = +svg.attr("height") - margin.top - margin.bottom,
         outerwidth = destwidth ? destwidth : 500,
@@ -31,12 +27,12 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
         innerwidth = outerwidth - margin.left - margin.right,
         g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    svg.attr("width",outerwidth).attr("height",outerheight);    
+    svg.attr("width", outerwidth).attr("height", outerheight);
 
     // if (selector == ".borrowing") {
     //     var parseTime = d3time.timeParse("%b");
     // } else 
-    if (interval == "month"){
+    if (interval == "month") {
         var parseTime = d3time.timeParse("%b %Y");
     } else if (interval == "day") {
         var parseTime = d3time.timeParse('%e-%b-%Y')
@@ -44,7 +40,7 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
 
     if (chartType == "line") {
         var x = d3scale.scaleTime()
-        .rangeRound([0, innerwidth]);
+            .rangeRound([0, innerwidth]);
     } else if (chartType == "bar") {
         var x = d3scale.scaleBand().rangeRound([0, innerwidth]).padding(0.1)
     }
@@ -59,29 +55,33 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
         .y(function (d) {
             return y(d.Value);
         });
-    
-        if (numberOfDataSeries > 1) {
-            var line2 = d3.line()
+
+    if (numberOfDataSeries > 1) {
+        var line2 = d3.line()
             .x(function (d) {
                 return x(d.Date);
             })
             .y(function (d) {
                 return y(d.Value2);
             });
-    
-        }
-    
-        if (numberOfDataSeries > 2) {
-            var line3 = d3.line()
+
+    }
+
+    if (numberOfDataSeries > 2) {
+        var line3 = d3.line()
             .x(function (d) {
-                return x(d.Date);
+                if (d.Value3){
+                    return x(d.Date);
+                }
             })
             .y(function (d) {
-                return y(d.Value3);
+                if (d.Value3) {
+                    return y(d.Value3);
+                }
             });
-    
-        }
-    
+
+    }
+
 
 
 
@@ -92,14 +92,20 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
             d.Value2 = parseFloat(d.Value2);
         }
         if (numberOfDataSeries > 2) {
-            d.Value3 = parseFloat(d.Value3);
+            if (!isNaN(parseFloat(d.Value3))) {
+                d.Value3 = parseFloat(d.Value3);
+            }
+            else {
+                delete d.Value3;
+            }
         }
 
     })
 
-    var junes = data.filter(function (d) {
-        return d.Date.getMonth() == 5;
-    })
+
+    // var junes = data.filter(function (d) {
+    //     return d.Date.getMonth() == 5;
+    // })
 
     if (interval == "month") {
         var june2016 = data.filter(function (d) {
@@ -124,9 +130,9 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
     if (chartType == "line") {
         x.domain(d3array.extent(data, function (d) { return d.Date; }));
     } else if (chartType == "bar") {
-        x.domain(data.map(function(d) { return d.Date; }));
+        x.domain(data.map(function (d) { return d.Date; }));
     }
-    
+
     if (zeroy == true) {
         y.domain([0, d3array.max(data, function (d) { return d.Value; })]);
     } else {
@@ -134,10 +140,10 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
             y.domain(d3array.extent(data, function (d) { return d.Value; }));
         }
         else if (numberOfDataSeries == 2) {
-            var combineddata = data.map(function(d) {
+            var combineddata = data.map(function (d) {
                 return d.Value;
-            }).concat(data.map(function(d){ return d.Value2}));
-            y.domain(d3array.extent(combineddata),function(d){return d})
+            }).concat(data.map(function (d) { return d.Value2 }));
+            y.domain(d3array.extent(combineddata), function (d) { return d })
         }
 
     }
@@ -146,30 +152,30 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
     if (selector != ".borrowing") {
 
         g.append("g")
-        .attr("class", "gv-brexit-tick")
-        .attr("transform", "translate(0," +innerheight +")")
-        .call(d3axis.axisBottom(x)
-            .tickValues(june2016.map(function (d) {
-                return d.Date;
-            })).
-            tickFormat(d3time.timeFormat('%b %Y'))
-            .tickSize(-innerheight))
-    // .select(".domain")
-    //   .remove();
+            .attr("class", "gv-brexit-tick")
+            .attr("transform", "translate(0," + innerheight + ")")
+            .call(d3axis.axisBottom(x)
+                .tickValues(june2016.map(function (d) {
+                    return d.Date;
+                })).
+                tickFormat(d3time.timeFormat('%b %Y'))
+                .tickSize(-innerheight))
+        // .select(".domain")
+        //   .remove();
 
 
-    g.append("g")
-        .attr("transform", "translate(0," +innerheight +")")
-        .call(d3axis.axisBottom(x)
-            .tickValues(june2017.map(function (d) {
-                return d.Date;
-            }))
-            .tickFormat(d3time.timeFormat('%b %Y')))
+        g.append("g")
+            .attr("transform", "translate(0," + innerheight + ")")
+            .call(d3axis.axisBottom(x)
+                .tickValues(june2017.map(function (d) {
+                    return d.Date;
+                }))
+                .tickFormat(d3time.timeFormat('%b %Y')))
     }
- 
+
     g.append("g")
         .attr("class", "gv-horizontal-grid")
-        .attr("transform","translate(-40,0)")
+        .attr("transform", "translate(-40,0)")
         .call(d3axis.axisLeft(y)
             .ticks(ticks)
             .tickSize(
@@ -178,79 +184,96 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
 
     g.append("g")
         .attr("class", "gv-zero-line")
-        .attr("transform","translate(-40,0)")
+        .attr("transform", "translate(-40,0)")
         .call(d3axis.axisLeft(y)
             .tickValues([0])
             .tickSize(
                 (-innerwidth) - 40
             )
-        .tickFormat(""))
+            .tickFormat(""))
+
+    if (selector == '.borrowing') {
+
+        g.append("g")
+            .attr("transform", "translate(0," + innerheight + ")")
+            .call(d3axis.axisBottom(x)
+                .tickValues(data.map(function (d) {
+                    return d.Date;
+                }))
+                .tickFormat(d3time.timeFormat('%b')))
+
+    }
 
 
-if (chartType == "line") {
-    g.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "#cc0a11")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 2)
-        .attr("d", line);
-}
-else if (chartType == "bar") {
-    g.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.Date); })
-      .attr("y", function(d) { return y(d.Value); })
-      .attr("width", x.bandwidth())
-      .attr("height", function(d) { return innerheight - y(d.Value); });
-} 
+    if (chartType == "line") {
+        g.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "#cc0a11")
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("stroke-width", 2)
+            .attr("d", line);
+    }
+    else if (chartType == "bar") {
+        g.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function (d) { return x(d.Date); })
+            .attr("y", function (d) { return y(d.Value); })
+            .attr("width", x.bandwidth())
+            .attr("height", function (d) { return innerheight - y(d.Value); })
+            .attr("fill", function (d) {
+                if (d.Date.getMonth() == 5 && d.Date.getFullYear() == 2016) {
+                    return "#b3b3b4";
+                } else { return "#cc0a11" }
+            });
+    }
     if (numberOfDataSeries > 1) {
         g.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "grey")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 2)
-        .attr("d", line2);
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "#056da1")
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("stroke-width", 2)
+            .attr("d", line2);
     }
 
     if (numberOfDataSeries > 2) {
+
+        var thirdyear = data.filter(function(d) {
+            return d.Value3;
+        })
         g.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "yellow")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 2)
-        .attr("d", line3);
+            .datum(thirdyear)
+            .attr("fill", "none")
+            .attr("stroke", "#ed6300")
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("stroke-width", 2)
+            .attr("d", line3);
     }
 
 
-        var graphdiv = document.querySelector(".gv-interactive"+selector);
-        if (destinationdiv != null) {
-            destinationdiv.appendChild(graphdiv);        
-        }
+    var graphdiv = document.querySelector(".gv-interactive" + selector);
+    if (destinationdiv != null) {
+        destinationdiv.appendChild(graphdiv);
+    }
 }
 
 d3request.json(dataurl, function (d) {
     var alldata = d.sheets;
-    drawlinechart(alldata.cpi, ".cpi", 3, true, "month","cpi","line",1);
-    drawlinechart(alldata.retail, ".retail", 5, false, "month","retail","line",1);
-    drawlinechart(alldata.rics, ".rics", 3, false, "month","rics","line",1);
-    drawlinechart(alldata.pmi, ".pmi", 5, false, "month","pmi","line",1);
-    drawlinechart(alldata.trade, ".trade", 7, false, "month", "trade","line",1);
-    drawlinechart(alldata.ftse100, ".ftse100", 7, false, "day", "ftse100","line",1);
-    drawlinechart(alldata.ftse250, ".ftse250", 7, false, "day", "ftse250","line",1);
-    drawlinechart(alldata.unemployment, ".unemployment", 7, true, "month", "unemployment","bar",1);
-    drawlinechart(alldata.wages, ".wages", 7, true, "month", "wages","bar",1);
-    drawlinechart(alldata.sterling, ".sterling", 7, false, "day", "sterling","line",2);
-    drawlinechart(alldata.borrowing, ".borrowing", 7, true, "month", "borrowing","line",3);
+    drawlinechart(alldata.cpi, ".cpi", 3, true, "month", "cpi", "line", 1);
+    drawlinechart(alldata.retail, ".retail", 5, false, "month", "retail", "line", 1);
+    drawlinechart(alldata.rics, ".rics", 3, false, "month", "rics", "line", 1);
+    drawlinechart(alldata.pmi, ".pmi", 5, false, "month", "pmi", "line", 1);
+    drawlinechart(alldata.trade, ".trade", 7, false, "month", "trade", "line", 1);
+    drawlinechart(alldata.ftse100, ".ftse100", 7, false, "day", "ftse100", "line", 1);
+    drawlinechart(alldata.ftse250, ".ftse250", 7, false, "day", "ftse250", "line", 1);
+    drawlinechart(alldata.unemployment, ".unemployment", 7, true, "month", "unemployment", "bar", 1);
+    drawlinechart(alldata.wages, ".wages", 7, true, "month", "wages", "bar", 1);
+    drawlinechart(alldata.sterling, ".sterling", 7, false, "day", "sterling", "line", 2);
+    drawlinechart(alldata.borrowing, ".borrowing", 7, true, "month", "borrowing", "line", 3);
 });
-
-
-
-

c5707d98622291bf8cd5dbf08b5a9b63f70cb244
Seán mór Clarke <sean.clarke@guardian.co.uk>
2019-09-27 15:28:31 +0100

    adds 2019 to ticks

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,7 @@ import config from './../../config.json'
 
 var dataurl = config.docDataUrl;
 
-function drawlinechart(data, selector, ticks, zeroy, interval, destination, chartType, numberOfDataSeries,columnNameArray) {
+function drawlinechart(data, selector, ticks, zeroy, interval, destination, chartType, numberOfDataSeries, columnNameArray) {
 
 
     var destinationdiv = window.parent.document.querySelector('figure[data-alt="' + destination + '"]');
@@ -43,17 +43,18 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
 
     data.map(function (d) {
         //only convert it to a date if it isn't already a date
-        if (Object.prototype.toString.call(d.Date) !== '[object Date]'){
+        if (Object.prototype.toString.call(d.Date) !== '[object Date]') {
             d.Date = parseTime(d.Date);
         }
         // convert values to numbers for as many values as there are
         // and delete any values that won't be coerced to numbers
         for (var i = 1; i < Object.keys(d).length; i++) {
-            if (!isNaN( parseFloat(d[Object.keys(d)[i]]))
-            ) {d[Object.keys(d)[i]] = parseFloat(d[Object.keys(d)[i]]);
-        }
+            if (!isNaN(parseFloat(d[Object.keys(d)[i]]))
+            ) {
+            d[Object.keys(d)[i]] = parseFloat(d[Object.keys(d)[i]]);
+            }
             else {
-                delete  d[Object.keys(d)[i]];
+                delete d[Object.keys(d)[i]];
             }
         }
 
@@ -91,7 +92,7 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
     if (numberOfDataSeries > 2) {
         var line3 = d3.line()
             .x(function (d) {
-                if (d[columnNameArray[2]]){
+                if (d[columnNameArray[2]]) {
                     return x(d.Date);
                 }
             })
@@ -106,7 +107,7 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
     if (numberOfDataSeries > 3) {
         var line4 = d3.line()
             .x(function (d) {
-                if (d[columnNameArray[3]]){
+                if (d[columnNameArray[3]]) {
                     return x(d.Date);
                 }
             })
@@ -121,7 +122,7 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
     if (numberOfDataSeries > 4) {
         var line5 = d3.line()
             .x(function (d) {
-                if (d[columnNameArray[4]]){
+                if (d[columnNameArray[4]]) {
                     return x(d.Date);
                 }
             })
@@ -133,16 +134,16 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
 
     }
 
- 
+
     if (interval == "month") {
-        
+
         try {
 
             var june2016 = data.filter(function (d) {
                 return d.Date.getMonth() == 5 && d.Date.getFullYear() == 2016;
-             })
-    
-            
+            })
+
+
         } catch (err) {
             console.log(data);
         }
@@ -173,6 +174,16 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
             return d.Date.getMonth() == 5 && d.Date.getFullYear() == 2018 && d.Date.getDate() == 23;
         })
     }
+    if (interval == "month") {
+        var june2019 = data.filter(function (d) {
+            return d.Date.getMonth() == 5 && d.Date.getFullYear() == 2019;
+        })
+    } else if (interval == "day") {
+        var june2019 = data.filter(function (d) {
+            return d.Date.getMonth() == 5 && d.Date.getFullYear() == 2019 && d.Date.getDate() == 24;
+        })
+    }
+
 
     if (chartType == "line") {
         x.domain(d3array.extent(data, function (d) { return d.Date; }));
@@ -184,7 +195,8 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
 
     if (zeroy == true) {
         y.domain([0, d3array.max(data, function (d) {
-             return d[columnNameArray[0]]; })]);
+            return d[columnNameArray[0]];
+        })]);
     } else {
         if (numberOfDataSeries == 1) {
             y.domain(d3array.extent(data, function (d) { return d[columnNameArray[0]]; }));
@@ -231,6 +243,14 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
                 }))
                 .tickFormat(d3time.timeFormat('%b %Y')));
 
+        g.append("g")
+            .attr("transform", "translate(0," + innerheight + ")")
+            .call(d3axis.axisBottom(x)
+                .tickValues(june2019.map(function (d) {
+                    return d.Date;
+                }))
+                .tickFormat(d3time.timeFormat('%b %Y')));
+
 
 
     }
@@ -259,8 +279,8 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
         g.append("g")
             .attr("transform", "translate(0," + innerheight + ")")
             .call(d3axis.axisBottom(x)
-            .ticks(5)
-            .tickFormat(d3time.timeFormat('%b')))
+                .ticks(5)
+                .tickFormat(d3time.timeFormat('%b')))
 
     }
 
@@ -269,10 +289,10 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
         g.append("path")
             .datum(data)
             .attr("fill", "none")
-            .attr("stroke", function() { 
+            .attr("stroke", function () {
                 if (selector == ".borrowing") {
                     return "#999999";
-                } else {return "#cc0a11" }  
+                } else { return "#cc0a11" }
             })
             .attr("stroke-linejoin", "round")
             .attr("stroke-linecap", "round")
@@ -298,10 +318,10 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
         g.append("path")
             .datum(data)
             .attr("fill", "none")
-            .attr("stroke", function() { 
+            .attr("stroke", function () {
                 if (selector == ".borrowing") {
                     return "#f5be2c";
-                } else {return "#056da1" }  
+                } else { return "#056da1" }
             })
             .attr("stroke-linejoin", "round")
             .attr("stroke-linecap", "round")
@@ -311,7 +331,7 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
 
     if (numberOfDataSeries > 2) {
 
-        var thirdyear = data.filter(function(d) {
+        var thirdyear = data.filter(function (d) {
             return d.Value3;
         })
         g.append("path")
@@ -326,7 +346,7 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
 
     if (numberOfDataSeries > 3) {
 
-        var fourthyear = data.filter(function(d) {
+        var fourthyear = data.filter(function (d) {
             return d.Value4;
         })
         g.append("path")
@@ -341,7 +361,7 @@ function drawlinechart(data, selector, ticks, zeroy, interval, destination, char
 
     if (numberOfDataSeries > 4) {
 
-        var fifthyear = data.filter(function(d) {
+        var fifthyear = data.filter(function (d) {
             return d.Value5;
         })
         g.append("path")
@@ -373,16 +393,16 @@ d3request.json(dataurl, function (d) {
     //data, selector, ticks, zeroy, interval, destination, chartType, numberOfDataSeries
     // ie: 'sheet', target div, number of ticks, set Y axis to zero?, timescale, target div again without the dot, type of chart, number of values, name of columns)
 
-       drawlinechart(alldata.cpi, ".cpi", 3, true, "month", "cpi", "line", 1, ["Value"]);
-      drawlinechart(alldata.tradeAndRetail, ".retail", 5, false, "month", "retail", "line", 1, ["retail"]);
-      drawlinechart(alldata.pmiRics, ".rics", 3, false, "month", "rics", "line", 1,["rics"]);
-       drawlinechart(alldata.pmiRics, ".pmi", 5, false, "month", "pmi", "line", 1,["pmi"]);
-      drawlinechart(alldata.tradeAndRetail, ".trade", 7, false, "month", "trade", "line", 1,["tradebalance"]);
-     drawlinechart(alldata.financials, ".ftse100", 6, false, "day", "ftse100", "line", 1,["ftse100"]);
-     drawlinechart(alldata.unemploymentWages, ".wages", 7, true, "month", "wages", "bar", 1,["wages"]);
-      drawlinechart(alldata.unemploymentWages, ".unemployment", 7, true, "month", "unemployment", "bar", 1,["unemployment"]);
-     drawlinechart(alldata.financials, ".sterling", 6, false, "day", "sterling", "line", 2,["dollar","euro"]);
-   drawlinechart(alldata.borrowing, ".borrowing", 7, true, "month", "borrowing", "line", 5,["Value","Value2","Value3","Value4","Value5"]);
-   drawlinechart(alldata.financials, ".ftse250", 6, false, "day", "ftse250", "line", 1,["ftse250"]);
+    drawlinechart(alldata.cpi, ".cpi", 3, true, "month", "cpi", "line", 1, ["Value"]);
+    drawlinechart(alldata.tradeAndRetail, ".retail", 5, false, "month", "retail", "line", 1, ["retail"]);
+    drawlinechart(alldata.pmiRics, ".rics", 3, false, "month", "rics", "line", 1, ["rics"]);
+    drawlinechart(alldata.pmiRics, ".pmi", 5, false, "month", "pmi", "line", 1, ["pmi"]);
+    drawlinechart(alldata.tradeAndRetail, ".trade", 7, false, "month", "trade", "line", 1, ["tradebalance"]);
+    drawlinechart(alldata.financials, ".ftse100", 6, false, "day", "ftse100", "line", 1, ["ftse100"]);
+    drawlinechart(alldata.unemploymentWages, ".wages", 7, true, "month", "wages", "bar", 1, ["wages"]);
+    drawlinechart(alldata.unemploymentWages, ".unemployment", 7, true, "month", "unemployment", "bar", 1, ["unemployment"]);
+    drawlinechart(alldata.financials, ".sterling", 6, false, "day", "sterling", "line", 2, ["dollar", "euro"]);
+    drawlinechart(alldata.borrowing, ".borrowing", 7, true, "month", "borrowing", "line", 5, ["Value", "Value2", "Value3", "Value4", "Value5"]);
+    drawlinechart(alldata.financials, ".ftse250", 6, false, "day", "ftse250", "line", 1, ["ftse250"]);
 
 });
